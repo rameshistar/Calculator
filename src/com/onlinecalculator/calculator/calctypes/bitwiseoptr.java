4bc89801e5cbbbbd2e3eda8f33e18790f2249529
istar <istar@192.168.100.10>
2017-05-25 08:41:26 +0530

    Bitwise by- Ramesh Korivi

---
+++ src/com/onlinecalculator/calculator/calctypes/bitwiseoptr.java
@@ -0,0 +1,14 @@
+package com.onlinecalculator.calculator.calctypes;
+
+public class bitwiseoptr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean a,b;
+		a=true;
+		b=false;
+		System.out.println(a & b);
+		
+	}
+
+}
