7d1a677c9d83d43896ea139e1923fdf2973d8a38
istar <istar@192.168.100.10>
2017-05-25 08:37:16 +0530

    Arithmetic by - Ramesh Korivi

---
+++ src/com/onlinecalculator/calculator/calctypes/arithmeticoptr.java
@@ -0,0 +1,16 @@
+package com.onlinecalculator.calculator.calctypes;
+
+public class arithmeticoptr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b;
+		a=10;
+		b=20;
+		int c= a+b;
+		
+		System.out.println(c);
+
+	}
+
+}
